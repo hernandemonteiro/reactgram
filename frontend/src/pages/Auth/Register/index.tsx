60e77ebae31a88912c1b8cbb33b236a77cff96b5
Hernande Monteiro <monteiro1998@live.com>
2023-01-20 14:58:13 -0300

    build: register page  and styling

--- frontend/src/pages/Auth/Register/index.tsx
+++ frontend/src/pages/Auth/Register/index.tsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { FormEvent } from "react";
 import "../Auth.css";
+import { Link } from "react-router-dom";
 
 function Register() {
-  return <div>Register</div>;
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+  }
+  return (
+    <div>
+      <h2>Reactgram</h2>
+      <p className="subtitle">Cadastre-se para ver as fotos dos seus amigos.</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Nome" />
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Senha" />
+        <input type="password" placeholder="Confirme a senha" />
+        <input type="submit" value="CADASTRAR" />
+      </form>
+      <p>
+        Já tem conta? <Link to={"/login"}>Clique aqui.</Link>
+      </p>
+    </div>
+  );
 }
 
 export default Register;
